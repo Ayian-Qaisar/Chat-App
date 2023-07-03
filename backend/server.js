862e3aade6c8404f49d03f37d4a14e6cac504017
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-07-03 13:15:36 +0500

    made some changes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.static(path.join("../frontend")));
-console.log(__dirname);
 
 const rooms = new Map(); // Map to store the rooms and their respective sockets
 const chatHistory = new Map(); // Map to store the chat history of each room
